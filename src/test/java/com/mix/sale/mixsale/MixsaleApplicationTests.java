fe3c71ef014467ab456f1fccb597bcba278d5274
yuttapols <myprofile.yuttapol@gmail.com>
2022-04-01 00:30:48 +0700

    Test Frist commit

---
+++ src/test/java/com/mix/sale/mixsale/MixsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mix.sale.mixsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
